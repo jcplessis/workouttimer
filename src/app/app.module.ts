85b0f27cf031f0e6f8d34612b34a429157a44f23
Jean-Christophe Plessis <jcplessis@serial.ch>
2017-01-26 14:00:06 +0100

    Adding material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { ExerciseComponent } from './exercise/exercise.component';
-import { WorkoutComponent } from './workout/workout.component';
+import {AppComponent} from './app.component';
+import {ExerciseComponent} from './exercise/exercise.component';
+import {WorkoutComponent} from './workout/workout.component';
+import {MaterialModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -16,9 +17,11 @@ import { WorkoutComponent } from './workout/workout.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
